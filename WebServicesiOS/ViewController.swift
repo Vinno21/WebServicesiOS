9f21e524c39655c17e37cd282caa6d7b0d8cb9e5
David <heysoydavid17>
2020-05-07 18:07:20 -0500

    Initial Commit

---
+++ WebServicesiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebServicesiOS
+//
+//  Created by David on 07/05/20.
+//  Copyright © 2020 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
